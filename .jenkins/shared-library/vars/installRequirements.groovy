813146f3400118811077b0a53688d6128d7f8ae6
Machteld Bögels <mabogels@microsoft.com>
2024-03-28 13:05:24 +0100

    add more shared library vars

--- .jenkins/shared-library/vars/installRequirements.groovy
+++ .jenkins/shared-library/vars/installRequirements.groovy
@@ -1,8 +1,8 @@
-def call() {
+def call(String requirements_type) {
     withPythonEnv('/usr/bin/python3.9') {
     sh """
     pip install setuptools wheel
-    pip install -r .jenkins/requirements/execute_job_requirements.txt
+    pip install -r .jenkins/requirements/${requirements_type}.txt
     pip install promptflow promptflow-tools promptflow-sdk jinja2 promptflow[azure] openai promptflow-sdk[builtins]
     """
     }
